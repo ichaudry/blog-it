a7c9195d59fa5b3412388bbb597844e69520511d
ichaudry <ibraheem27496@gmail.com>
2020-05-30 22:34:43 -0700

    Completed follow feature.

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,6 +12,17 @@ exports.sharedProfileData = async function (req, res, next) {
 
     req.isVisitorsProfile = isVisitorsProfile
     req.isFollowing= isFollowing
+
+    //retrieve post, follower, following counts
+    let postCountPromise = Post.countPostsByAuthor(req.profileUser._id)
+    let followerCountPromise = Follow.countFollowersById(req.profileUser._id)
+    let followingCountPromise = Follow.countFollowingById(req.profileUser._id)
+    let [postCount, followerCount, followingCount] = await Promise.all([postCountPromise, followerCountPromise, followingCountPromise])
+    
+    req.postCount = postCount
+    req.followerCount = followerCount
+    req.followingCount = followingCount
+
     next()
 }
 
@@ -73,9 +84,11 @@ exports.register = function(req , res) {
     })
 }
 
-exports.home = function(req , res) {
+exports.home = async function(req , res) {
     if(req.session.user){
-        res.render('home-dashboard')
+        // Fetch feed of posts for current user
+        let posts = await Post.getFeed(req.session.user._id)
+        res.render('home-dashboard', {posts: posts})
     } else {
         res.render('home-guest', {regErrors: req.flash('regErrors')})   
     }
@@ -94,14 +107,49 @@ exports.profilePostsScreen = function(req, res) {
     //Get posts for certain autor id using post model
     Post.findPostByAuthorId(req.profileUser._id).then(function(posts){
         res.render('profile', {
+            currentPage: "posts",
             posts: posts,
             profileUsername: req.profileUser.username,
             profileAvatar: req.profileUser.avatar,
             isFollowing: req.isFollowing,
-            isVisitorsProfile: req.isVisitorsProfile
+            isVisitorsProfile: req.isVisitorsProfile,
+            counts: {postCount: req.postCount, followerCount: req.followerCount, followingCount: req.followingCount}
         })
     }).catch(function(){
         res.render("404")
     })
 }
 
+exports.profileFollowersScreen = async function(req, res){
+    try {
+        let followers = await Follow.getFollowersById(req.profileUser._id)
+        res.render('profile-followers', {
+            currentPage: "followers",
+            followers: followers,
+            profileUsername: req.profileUser.username,
+            profileAvatar: req.profileUser.avatar,
+            isFollowing: req.isFollowing,
+            isVisitorsProfile: req.isVisitorsProfile,
+            counts: {postCount: req.postCount, followerCount: req.followerCount, followingCount: req.followingCount}
+        })
+    } catch {
+      res.render('404')
+    }
+  }
+
+  exports.profileFollowingScreen = async function(req, res){
+    try {
+        let following = await Follow.getFollowingById(req.profileUser._id)
+        res.render('profile-following', {
+            currentPage: "following",
+            following: following,
+            profileUsername: req.profileUser.username,
+            profileAvatar: req.profileUser.avatar,
+            isFollowing: req.isFollowing,
+            isVisitorsProfile: req.isVisitorsProfile,
+            counts: {postCount: req.postCount, followerCount: req.followerCount, followingCount: req.followingCount}
+        })
+    } catch {
+      res.render('404')
+    }
+  }
